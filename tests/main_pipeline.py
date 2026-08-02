1ff568e18f0564a2505d6c8515130ceb5e29e5dc
Sebástian Andrés Narváez Salcedo <snarvaezwrm@gmail.com>
2026-08-02 17:15:38 -0500

    test: dataframe length

--- tests/main_pipeline.py
+++ tests/main_pipeline.py
@@ -5,8 +5,8 @@ from prepare_wfs_data.plot import plot_wildfires_clusters
 
 
 if __name__ == "__main__":
-    gdf = gpd.read_file("data/wfs-area-export-Atlantico_2026-01-01-2026-08-02.geojson")
-    gdf = prepare_data(gdf)
-    print(gdf.info())
-    plot_wildfires_clusters(gdf)
+    gdf_1 = gpd.read_file("data/wfs-area-export-Atlantico_2026-01-01-2026-08-02.geojson")
+    gdf_2 = prepare_data(gdf_1)
+    assert len(gdf_1.geometry) >= len(gdf_2.geometry)
+    plot_wildfires_clusters(gdf_2)
     plt.show()
